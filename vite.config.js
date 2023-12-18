f548947d0e72268999dddc5e90d9fca93f89f8d9
AnnaShliapkina <deer1anna@gmail.com>
2023-12-18 18:17:38 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/UA_deminer_IconStyle",
+  base: "/UA_deminer_IconStyle/",
   plugins: [react()],
 });
